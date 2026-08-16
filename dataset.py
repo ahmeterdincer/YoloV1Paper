07f2383c6c8df8c8ca790ee2bf9753a131570fee
Ahmet Erdinçer <ahmeterdincer889@gmail.com>
2026-08-16 22:35:40 +0300

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,7 +1,8 @@
 from torchvision.datasets import VOCDetection
 from torch.utils.data import Subset
 import os
-
+from torch.utils.data import random_split
+from torchvision.transforms import transforms
 voc_root = "./data"
 
 def data_load(voc_root):
@@ -22,10 +23,39 @@ def data_load(voc_root):
     )
     
     print(f"train length: {len(train_dataset)}\n test length: {len(test_dataset)}")
-    train_data_cutted = Subset(train_dataset, range(int(len(train_dataset)*0.6)))
-    test_data_cutted = Subset(test_dataset, range(int(len(test_dataset)*0.15)))
+    
+    train_data_cutted, _ = random_split(train_dataset, lengths=[0.6,0.4])
+    test_data_cutted, _ = random_split(test_dataset, lengths=[0.15,0.85])
     print("Cutted sets")
     print(len(train_data_cutted))
     print(len(test_data_cutted))
+    return train_data_cutted, test_data_cutted
+
+def feature_extracture():
+    train_transforms = transforms.Compose(
+        [
+            transforms.Resize(448,448),
+            transforms.RandomAffine(
+                degrees=0,
+                translate=(0.2,0.2),
+                scale=(0.8,1.2)),
+            transforms.ColorJitter(
+                brightness=0.2,
+                saturation=0.2),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225]),
+            transforms.ToTensor()
+        ]
+    )
+    test_transforms = transforms.Compose([
+    transforms.Resize((448, 448)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225])
+    ])
+
+
 if __name__ == "__main__":
-    data_load(voc_root=voc_root)
+    train_data, test_data = data_load(voc_root=voc_root)
