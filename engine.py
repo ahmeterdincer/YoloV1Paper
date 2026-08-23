bf12296a66c746040d378dfdedae88f8fed075b7
Ahmet Erdinçer <ahmeterdincer889@gmail.com>
2026-08-23 23:34:44 +0300

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-
+import tqdm
 
 
 class TrainTestSteps(nn.Module):
@@ -12,11 +12,36 @@ class TrainTestSteps(nn.Module):
         model.train()
 
         train_loss =0
-        
+        mean_loss=[]
+        loop = tqdm(dataloader, leave=True)
         for batch, (x,y) in enumerate(dataloader):
-            x,y = 
+            x,y = x.to(device), y.to(device)
             y_pred = model(x)
 
-            loss_logits = loss_fn(y_pred,y)
-            train_loss+= loss_logits.item()
+            loss = loss_fn(y_pred,y)
+            train_loss+= loss.item()
 
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            mean_loss.append(loss.item())
+            loop.set_postfix(loss=loss.item())
+
+        return sum(mean_loss)/len(mean_loss)
+
+    def test_step(self, model:nn.Module, dataloader:DataLoader,loss_fn:nn.Module,device:torch.device):
+        model.eval()
+        test_loss=0
+        
+        with torch.inference_mode():
+            for batch,(x,y) in enumerate(dataloader):
+                x,y = x.to(device), y.to(device)
+                test_pred = model(x)
+
+                loss = loss_fn(test_pred,y)
+                test_loss += loss.item()
+
+                test_pred_labels = test_pred.argmax(dim=1)
+            
+            test_loss = test_loss/len(dataloader) 
+            return test_loss
